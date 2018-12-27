9fa4b3f75c2d6302cc2efde13f43f3460155296f
viragumathe5 <44305195+viragumathe5@users.noreply.github.com>
2018-12-27 10:17:56 +0530

    Create url.py

---
+++ url.py
@@ -0,0 +1,24 @@
+import sys
+from tkinter import *
+from webbrowser import *
+
+
+
+tab = Tk()
+g1int= StringVar()
+tab.title("MINI URL")
+tab.geometry('300x150')
+
+
+def opn_url():
+    g2int = g1int.get()
+    type(g2int)
+    webbrowser.open(g2int)
+      
+
+
+lbl_1 = Label(tab, text = "GET SITE").pack()
+g_entry = Entry(tab, textvariable = g1int).pack()
+button_1 = Button(tab, text = "SEARCH", fg = "red", command = opn_url).pack()
+
+tab.mainloop()
