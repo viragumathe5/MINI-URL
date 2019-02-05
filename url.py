506eb8cbc3cf5651f98a2e99d64b6431f315e608
viragumathe5 <44305195+viragumathe5@users.noreply.github.com>
2019-02-05 12:13:56 +0530

    Update url.py

--- url.py
+++ url.py
@@ -2,13 +2,14 @@ import sys
 from tkinter import *
 from webbrowser import *
 
-
+#----------------------------------------------------------------------------------------------------------------
 
 tab = Tk()
 g1int= StringVar()
 tab.title("MINI URL")
 tab.geometry('300x150')
 
+#----------------------------------------------------------------------------------------------------------------
 
 def opn_url():
     g2int = g1int.get()
@@ -23,8 +24,8 @@ def opn_url():
         tab_1.geometry('150x150')
         lbl_2 = Label(tab_1, text = "Invalid inputs").pack()
       
-
-
+#----------------------------------------------------------------------------------------------------------------
+#GUI with tkinter
 lbl_1 = Label(tab, text = "GET SITE").pack()
 g_entry = Entry(tab, textvariable = g1int).pack()
 button_1 = Button(tab, text = "SEARCH", fg = "red", command = opn_url).pack()
